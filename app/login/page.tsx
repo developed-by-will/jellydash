9a523a24ebf89ae642480f32c6169214173e1bdf
developerwill <info@wilsongomes.me>
2025-07-31 14:12:07 +0000

    added breeze-ui toast component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,31 @@
 'use client';
 
+import { useToast } from '@/components/breeze-ui/toast/hooks/use-toast';
+import { SiJellyfin } from 'react-icons/si';
+import backgroundImage from '../../public/Splashscreen.jpeg';
+import logo from '../../public/logo.webp';
 import LoginPage01 from './components/form';
 
 export default function Login() {
+  const { toast } = useToast();
+
   async function signIn() {
-    alert('Sign in logic');
+    toast({
+      description: 'description',
+      title: 'title'
+    });
   }
 
   return (
     <LoginPage01
-      customBtnColor="bg:background"
-      customLabel="Sign In"
-      customIcon=""
+      customBtnColor="bg-indigo-600 hover:bg-indigo-700"
+      customLabel="Sign In with Jellyfin"
+      backgroundImage={backgroundImage}
+      companyLogo={logo}
+      customIcon={<SiJellyfin />}
       formWidth={300}
-      providers={['custom', 'google']}
+      providers={['custom']}
+      title="Login with Jellyfin"
       handleLogin={[() => signIn()]}
     />
   );
