e1b795de6f35fd7e1c0a03a90e69a3ba42d06980
developerwill <info@wilsongomes.me>
2025-07-31 18:28:59 +0000

    Hydrate.tsx update

--- app/Hydrate.tsx
+++ app/Hydrate.tsx
@@ -1,13 +1,40 @@
 'use client';
 
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { StrictMode, useEffect, useState } from 'react';
 
 export default function Hydrate({ children }: Readonly<{ children: React.ReactNode }>) {
   const [isHydrated, setIsHydrated] = useState(false);
+  const [localStorageInit, setLocalStorageInit] = useState<Storage>();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  });
 
   useEffect(() => {
     setIsHydrated(true);
+    setLocalStorageInit(window.localStorage);
   }, []);
 
-  return <StrictMode>{isHydrated && children}</StrictMode>;
+  return (
+    <StrictMode>
+      {isHydrated && (
+        <PersistQueryClientProvider
+          client={queryClient}
+          persistOptions={{
+            persister: createAsyncStoragePersister({
+              storage: localStorageInit
+            })
+          }}
+        >
+          {children}
+        </PersistQueryClientProvider>
+      )}
+    </StrictMode>
+  );
 }
